5489007e06f1ae18de6251e9d35da161c6ac39d3
rrocker89437 <ramiah.rocker@yahoo.com>
2025-02-28 04:33:48 -0800

    Finished home page
    havent decided if i want to keep the very minimal google clone but until i come up with something better... it stays

--- src/app.js
+++ src/app.js
@@ -3,15 +3,18 @@ import "../scss/style.scss";
 const tabData = {
   home: `
   <section id="home" class="home flex jc-c ai-c">
+    <button id="toggle-btn" class="toggle-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#919191" d="M297.2 248.9C311.6 228.3 320 203.2 320 176c0-70.7-57.3-128-128-128S64 105.3 64 176c0 27.2 8.4 52.3 22.8 72.9c3.7 5.3 8.1 11.3 12.8 17.7c0 0 0 0 0 0c12.9 17.7 28.3 38.9 39.8 59.8c10.4 19 15.7 38.8 18.3 57.5L109 384c-2.2-12-5.9-23.7-11.8-34.5c-9.9-18-22.2-34.9-34.5-51.8c0 0 0 0 0 0s0 0 0 0c-5.2-7.1-10.4-14.2-15.4-21.4C27.6 247.9 16 213.3 16 176C16 78.8 94.8 0 192 0s176 78.8 176 176c0 37.3-11.6 71.9-31.4 100.3c-5 7.2-10.2 14.3-15.4 21.4c0 0 0 0 0 0s0 0 0 0c-12.3 16.8-24.6 33.7-34.5 51.8c-5.9 10.8-9.6 22.5-11.8 34.5l-48.6 0c2.6-18.7 7.9-38.6 18.3-57.5c11.5-20.9 26.9-42.1 39.8-59.8c0 0 0 0 0 0s0 0 0 0s0 0 0 0c4.7-6.4 9-12.4 12.7-17.7zM192 128c-26.5 0-48 21.5-48 48c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-44.2 35.8-80 80-80c8.8 0 16 7.2 16 16s-7.2 16-16 16zm0 384c-44.2 0-80-35.8-80-80l0-16 160 0 0 16c0 44.2-35.8 80-80 80z"/></svg>
+    </button>
     <h1>Google Clone</h1>
-    <button id="toggle-btn" class="toggle-btn">Toggle Dark Mode</button>
     <div class="google-search">
-      <input type="text" id="searchBox">
-      <span class="placeholder">Search Google or type a URL</span>
+      <input placeholder="Search Google or type a URL" type="text" id="searchBox">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
     </div>
-    <br>
-    <button id="search-btn" class="search-btn">Search</button>
+    <div class="search-btns flex gap">
+      <button id="search-btn" class="search-btn">Search</button>
+      <button id="search-btn" class="lucky-btn">I'm Feeling Lucky</button>
+    </div>
   </section>
   `,
 
@@ -307,6 +310,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  // Trigger search when the Enter key is pressed
+  if (searchBox) {
+    searchBox.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        console.log("Enter pressed");
+        searchGoogle();
+      }
+    });
+  } else {
+    console.error("Search box not found!");
+  }
+
+  // Trigger search on button click
   if (searchbtn) {
     searchbtn.addEventListener("click", () => {
       console.log("Clicked");
@@ -316,6 +332,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("Search button not found!");
   }
 
+  // Toggle dark mode on button click
   if (togglebtn) {
     togglebtn.addEventListener("click", () => {
       toggleDarkMode();
